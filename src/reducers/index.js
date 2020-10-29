d42e16596325278a459ba8202a6795ebf0b216e8
zmatthews33 <zmatt33@gmail.com>
2020-10-29 12:01:01 -0500

    added reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [
+        { title: "Smells Like Teen Spirit", duration: "3:00",},
+        {title: "No Scrubs", duration: "3:05",},
+        {title: "Drop It Like It's Hot", duration: "3:30",},
+        {title: "Baby Got Back", duration: "2:55",}
+    ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+ if (action.type === "SONG_SELECTED") {
+     return action.payload;
+ }
+ return selectedSong
+}
