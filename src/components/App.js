c134deef9020ada4a668756538e30f3c921e389c
zmatthews33 <zmatt33@gmail.com>
2020-10-29 15:12:53 -0500

    created SongList component and add connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import SongList from './SongList'
 
 const App = () =>{
-    return <div>App</div>
-
+    return <div>
+        <SongList/>
+    </div>
+ 
 };
 
 export default App
