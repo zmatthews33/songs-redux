dfe11edbfe0a6da65480590bed6e4b5c79674cbc
zmatthews33 <zmatt33@gmail.com>
2020-10-30 17:04:45 -0500

    configured

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunk from 'redux-thunk';
+import persistState from 'redux-localstorage'
 import App from './components/App'
 import reducers from './reducers'
 
+const enhancer = compose(
+    applyMiddleware(thunk),
+    persistState(null, null),
+  )
+const store = createStore(reducers, enhancer);
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
     <App/>
     </Provider>
     , document.querySelector('#root')
